718733da2c5c3003170725227e4e5bae03143c7f
Jeff Radaszkiewicz <jr926@drexel.edu>
2015-11-13 16:55:17 -0500

    linking index.js

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var fs= require('fs');
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	//res.render('index', { title: 'iot' });
-	fs.readFile('data.txt',"utf-8",function(err,data){
-		if (err){
-		throw err;
-		}
-		console.log(typeof data);
-		res.send(data);
-	})
+    res.render('index', { title: 'iot' });
+    fs.readFile('data.txt', "utf-8", function(err, data) {
+        if (err) {
+            res.send(err);
+        }
+        data=JSON.parse(data);
+        console.log(typeof data);
+        //res.send(data);
+    
+    })
 });
 
 module.exports = router;
