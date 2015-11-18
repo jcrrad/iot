94b8ca5df7a176a2a1b25e47e23299aa12ee53da
jeff <jr926@drexel.edu>
2015-11-17 20:02:59 -0500

    started serial connection with read/writes to files

--- routes/index.js
+++ routes/index.js
@@ -15,40 +15,16 @@ router.get('/previous/', function(req, res, next) {
         if (err) {
             res.send(err);
         }
-        data = JSON.parse(data);
-        //     res.send(data);
-        var time = new Date().getTime();
-        var a = [];
-        var b = [];
-        for (var x = 10; x > 0; x--) {
-            a.push([time - x * 1000, Math.random() * 10]);
-            b.push([time - x * 1000, Math.random() * 10]);
-        }
-        var ans = [{
-            "name": "A",
-            "data": a
-        }, {
-            "name": "B",
-            "data": b
-        }];
-        res.send(ans);
+        res.send(data);
     })
 })
 router.get('/current', function(req, res, next) {
-    var offset = new Date().getTimezoneOffset();
-    offset = offset * 1000 * 60 * 60;
-    offset = 0;
-    var time = new Date().getTime() - offset;
-    console.log()
-    var ans = [
-        [time, Math.random() * 10],
-        [time, Math.random() * 10]
-    ]
-    res.send(ans)
-    var xbeeConfig = {
-            baudrate: 9600
+  
+    fs.readFile('current.txt', "utf-8", function(err, data) {
+        if (err) {
+            res.send(err);
         }
-        //var xbee = new SerialPort("/dev/ttyUSB0", xbeeConfig);
-
+        res.send(data);
+    })
 })
 module.exports = router;
